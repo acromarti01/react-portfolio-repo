2c1ef9d714237b976138295c6ae8472381da2fe1
Anthony Cromartie <acromarti@gmail.com>
2021-10-11 11:12:19 -0500

    updated Aboutme/index.js with bio paragraph

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -3,11 +3,33 @@ import '../../styles/AboutMe.css';
 
 
 const AboutMe = () => {
-  return (
-    <div className="main-container">
-
-    </div>
-  );
+    return (
+        <div className="main-container">
+            <div className="inner-container">
+                <div className="title-container">
+                    <p>about me</p>
+                </div>
+                <div className="image-container"></div>
+                <div className="description-container">
+                    <p className="bio">
+                        I am a student at Rice University Coding Boot Camp currently living in Houston, Texas. I graduated from
+                        Michigan
+                        State University with a bachelor’s degree in computer science engineering. Some projects I have worked
+                        on include
+                        building a database and a gas mileage calculator for the Houston Health Department.
+                    </p>
+                    <p className="bio">
+                        My fascination for computers
+                        started around late middle school. Before I knew it, I would spend hours just surfing many websites
+                        looking at their
+                        creative styling and wondering how developers were able to create something so captivating. Some things
+                        I like to do
+                        in my free time is running, watching sports and anime, and playing video games.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default AboutMe;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
-
-
+import Form from '../components/Contact';
 
 
 const Contact = () => {
-
   return (
-//     <main>
-      
-  <div className="row">
-    <div className="col s12 m6">
-      <div className="card blue-grey darken-1">
-        <div className="card-content white-text">
-          <span className="card-title">Card Title</span>
-          <p>This is the CONTACT page</p>
-        </div>
-        
-      </div>
-    </div>
-  </div>
-            
-//     </main>
+    <Form />
   );
 };
 
