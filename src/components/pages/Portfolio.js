c1a57d04e1b7b059fa70f566f352358a9cb2dfef
Anthony Cromartie <acromarti@gmail.com>
2021-10-15 16:13:54 -0500

    added project and repo links in Portfolio.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../styles/Portfolio.css';
+import gitHubIcon from '../../assets/images/icons/github-mark-dark-32px.png'
 
 export default function Portfolio() {
     return (
@@ -8,19 +9,80 @@ export default function Portfolio() {
                 <div className="title-container">
                     <p>Portfolio</p>
                 </div>
-                <div className="por-container">
-                    <div className="grid-container">
-                        <div class="item item a1 wrap">
+                <div className="portfolio">
+                    <div className="grid">
+                        <div className="box box1">
                             <div className="img-description">
-                                <a className="project-links" href="https://last-moment-savers.herokuapp.com">The link</a>
-                            </div>
-                            
+                                <div className="project-title-container">
+                                    <a className="project-title" href="https://last-moment-savers.herokuapp.com">
+                                        last moment savers
+                                    </a>          
+                                    <a href="https://github.com/avidrunner87/last-moment-savers.git">
+                                        <img src={gitHubIcon} alt="GitHub Icon" />
+                                    </a>
+                                </div>                                                               
+                            </div>                            
                         </div>
-                        <div className="item item a2 wrap"></div>
-                        <div className="item item a3 wrap"></div>
-                        <div className="item item a4 wrap"></div>
-                        <div className="item item a5 wrap"></div>
-                        <div className="item item a6 wrap"></div>
+                        <div className="box box2">
+                            <div className="img-description">
+                                <div className="project-title-container">
+                                    <a className="project-title" href="https://avidrunner87.github.io/small-talk">
+                                        small talk
+                                    </a>          
+                                    <a href="https://github.com/avidrunner87/small-talk.git">
+                                        <img src={gitHubIcon} alt="GitHub Icon" />
+                                    </a>
+                                </div>                                                               
+                            </div>                            
+                        </div>
+                        <div className="box box3">
+                            <div className="img-description">
+                                <div className="project-title-container">
+                                    <a className="project-title" href="https://drive.google.com/file/d/1sS0fzzeiJPgGbSPonGLsN4F_63C-2OGS/view">
+                                        employee tracker
+                                    </a>          
+                                    <a href="https://github.com/acromarti01/employee-tracker-repo.git">
+                                        <img src={gitHubIcon} alt="GitHub Icon" />
+                                    </a>
+                                </div>                                                               
+                            </div>                            
+                        </div>
+                        <div className="box box4">
+                            <div className="img-description">
+                                <div className="project-title-container">
+                                    <a className="project-title" href="https://drive.google.com/file/d/1DjjMiBH7ANFSefByHbgRr2QNnzFbA11A/view">
+                                        team profile generator
+                                    </a>          
+                                    <a href="https://github.com/acromarti01/team-profile-generator.git">
+                                        <img src={gitHubIcon} alt="GitHub Icon" />
+                                    </a>
+                                </div>                                                               
+                            </div>                            
+                        </div>
+                        <div className="box box5">
+                            <div className="img-description">
+                                <div className="project-title-container">
+                                    <a className="project-title" href="https://acromarti01.github.io/weather-dashboard-repo/">
+                                        weather dashboard
+                                    </a>          
+                                    <a href="https://github.com/acromarti01/weather-dashboard-repo.git">
+                                        <img src={gitHubIcon} alt="GitHub Icon" />
+                                    </a>
+                                </div>                                                               
+                            </div>                            
+                        </div>
+                        <div className="box box6">
+                            <div className="img-description">
+                                <div className="project-title-container">
+                                    <a className="project-title" href="https://acromarti01.github.io/work-day-scheduler-repo/">
+                                        work day scheduler
+                                    </a>          
+                                    <a href="https://github.com/acromarti01/work-day-scheduler-repo.git">
+                                        <img src={gitHubIcon} alt="GitHub Icon" />
+                                    </a>
+                                </div>                                                               
+                            </div>                            
+                        </div>
                     </div>
                 </div>
             </div>           
